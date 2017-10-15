baf0008f791326983efa14d0625a456392a6bf7c
Nicole Campbell <nscampbell303@gmail.com>
2017-10-15 18:54:29 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -10,6 +10,14 @@ function frameIt() {
   })
 }
 
+function pressIt() {
+  $("form").on("keydown", function() {
+    if ($( "input:first" ).val() === 71) {
+      alert("g is pressed")
+    }
+  })
+}
+
 $(document).ready(function(){
 
 return getIt()
