5717dc3b60cbd8c28b6b8be47c9cb5f18265c830
Nicole Campbell <nscampbell303@gmail.com>
2017-10-15 19:01:55 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -21,7 +21,7 @@ function pressIt() {
 function submitIt() {
   $("form").on('submit', function() {
     if ($("input:first").val() == "correct") {
-      alert("Your form is going to be submitted.")
+      alert("Your form is going to be submitted now.")
       return;
     }
     alert("You entered the wrong value.")
